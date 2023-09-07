c76ab244416f84b89becf1006b668b830596b216
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-07 15:34:24 +0200

    [UPDATE]: file script

--- js/script.js
+++ js/script.js
@@ -4,9 +4,15 @@ Calcolo del prezzo del biglietto del treno.
 
 1. Creare due variabili con prompt, una dove chiede il numero di chilometri da percorrere, una dove chiede l'età dell'utente;
 
+
 inserire all'interno delle variabili parsInt per assicurare un valore numerico.
 
 2. Creare una variabile con il prezzo al chilometro;
+
+Creare due variabili con il valore dello sconto, uno per i minorenni, uno per gli over 65;
+
+3. Applicare gli sconti in base all'età;
+
 
 
 */
@@ -22,9 +28,25 @@ const age = 20 //parseInt(prompt('Quanti anni hai?'));
 
 
 // 2.
+
 const price = 0.21;
 
+const discountUnderAge = 0.20;
+
+const discountOverAge = 0.40;
+
 let ticket = kmTravel * price;
+
+let ticketUnderAge = ticket - (ticket * discountUnderAge);
+
+let tickeOverAge = ticket - (ticket * discountOverAge);
+
+
+
+
+
+// 3.
+
 
 
 
@@ -32,3 +54,5 @@ let ticket = kmTravel * price;
 console.log(kmTravel);
 console.log(age);
 console.log(ticket);
+console.log(ticketUnderAge);
+console.log(ticketOverAge);
