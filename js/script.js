f06eceecbd9a860d427cf82bb972879d842c7c9a
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-07 16:09:14 +0200

    [UPDATE]: file script

--- js/script.js
+++ js/script.js
@@ -17,50 +17,43 @@ Creare due variabili con il valore dello sconto, uno per i minorenni, uno per gl
 
 */
 
-
-
 // 1.
 
-const kmTravel = 10 //parseInt(prompt('Quanti chilometri desideri percorrere?'));
+const kmTravel = 10; //parseInt(prompt('Quanti chilometri desideri percorrere?'));
 
-const age = 20 //parseInt(prompt('Quanti anni hai?'));
+const age = 65; //parseInt(prompt('Quanti anni hai?'));
 
 let message;
-
-
 
 // 2.
 
 const price = 0.21;
 
-const discountUnderAge = 0.20;
+const discountUnderAge = 0.2;
 
-const discountOverAge = 0.40;
+const discountOverAge = 0.4;
 
 let ticket = kmTravel * price;
 
-let ticketUnderAge = ticket - (ticket * discountUnderAge);
+let ticketUnderAge = ticket - ticket * discountUnderAge;
 
-let ticketOverAge = ticket - (ticket * discountOverAge);
-
-
-
+let ticketOverAge = ticket - ticket * discountOverAge;
 
 // 3.
 
-
 if (age <= 17) {
-message = `Il prezzo del biglietto è pari a ${ticketUnderAge}`;
-}else if (age >=65) {
-    message = `Il prezzo del biglietto è pari a ${ticketOverAge}`;
-}else{
-    message = `Il prezzo del biglietto è pari a ${ticket}`;
+  message = `Il prezzo del biglietto è pari a € ${ticketUnderAge}, con uno sconto applicato del 20%.`;
+} else if (age >= 65) {
+  message = `Il prezzo del biglietto è pari a € ${ticketOverAge}, con uno sconto applicato del 40%.`;
+} else {
+  message = `Il prezzo del biglietto è pari a € ${ticket}.`;
 }
-
-
 
 console.log(kmTravel);
 console.log(age);
 console.log(ticket);
 console.log(ticketUnderAge);
 console.log(ticketOverAge);
+
+
+document.getElementById('output').innerHTML = message;
