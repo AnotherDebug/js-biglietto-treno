b8d9450a049eac77d13dc97f76311d1b327b963d
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-07 14:20:54 +0200

    [UPDATE]: file script

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 Calcolo del prezzo del biglietto del treno.
 ===========================================
 
-1.
+1. Creare due variabili con prompt, una dove chiede il numero di chilometri da percorrere, una dove chiede l'età dell'utente;
 
 
 */
+
+
+
+// 1.
+
+const km = prompt('Quanti chilometri desideri percorrere?');
+
+const age = prompt('Quanti anni hai?');
